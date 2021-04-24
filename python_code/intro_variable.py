c1b1fe3848fd678aa6ae212232f1dea162bbbd4d
Seyram Kwame Attah <seyramkwameattah@gmail.com>
2021-04-24 00:21:09 +0000

    adding the intro variable file

---
+++ python_code/intro_variable.py
@@ -0,0 +1,7 @@
+#Date: 24th April 2021
+#Description: Create a python script to save your name and age.
+
+my_name = 'Seyram Kwame Attah'
+my_age = 37
+print('My age will be {} in 10 years time'.format(my_age + 10))
+
